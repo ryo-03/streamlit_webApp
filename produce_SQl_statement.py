6a413a67c9bf5130853086e798ff18e027809c6f
Ryosei Okayama <ryoseiokayama@ryoseis-air.mynetworksettings.com>
2023-08-24 02:02:22 -0400

    Good Work

--- produce_SQl_statement.py
+++ produce_SQl_statement.py
@@ -1,63 +1,88 @@
+def order_statement():
+    return ' ORDER BY tree.id'
+
 def group_statement():
-    return "GROUP BY tree.id"
+    return ' GROUP BY tree.id'
+
+def where_statement(query_dict):
+    query = ""
+    where = 0
+    OR = 0
+    if query_dict["english"]:
+        li = query_dict["english"]
+        if where == 0:
+            query += " WHERE"
+            where = 1
+        if OR == 1:
+            query += " OR"
+        else:
+            OR = 1
+        for i in range(len(li)):
+            if i > 0:
+                q += " OR"
+            query += " english_name LIKE '%{}%'".format(li[i])
+
+    return query
+
 
 def join_statement():
     return """
-            FROM tree 
-            LEFT JOIN regional_spelling
-			    ON regional_spelling.tree_id = tree.id
-            JOIN climatic_zone
-	            ON climatic_zone.tree_id = tree.id
-            JOIN climatic
-	            ON climatic.id = climatic_zone.climatic_id
-            JOIN utility_usage
-                ON utility_usage.tree_id = tree.id
-            JOIN utility
+            FROM tree
+            LEFT JOIN regional_spelling 
+                ON regional_spelling.tree_id = tree.id 
+            INNER JOIN climatic_zone 
+                ON climatic_zone.tree_id = tree.id 
+            INNER JOIN climatic 
+                ON climatic.id = climatic_zone.climatic_id 
+            INNER JOIN utility_usage 
+                ON utility_usage.tree_id = tree.id 
+            INNER JOIN utility 
                 ON utility.id = utility_usage.utility_id 
             """
             
 
 def data_to_view(select_list, utility_list):
-    statement = "SELECT botanical_name AS `Botanical Name`"
+    statement = 'SELECT botanical_name AS "Botanical Name"'
 
     if "Somali" in select_list:
-        statement += ", somali_name AS `Somali Name`"
+        statement += ', somali_name AS "Somali Name"'
     if "Arabic" in select_list:
-        statement += ", arabic_name AS `Arabic Name`"
+        statement += ', arabic_name AS "Arabic Name"'
     if "English" in select_list:
-        statement += ", english_name AS `English Name`"
+        statement += ', english_name AS "English Name"'
     if "Other Regional Spelling" in select_list:
-        statement += ", GROUP_CONCAT(DISTINCT spelling SEPARATOR ', ') AS `Other Regional Spelling`"
+        statement += """, STRING_AGG(DISTINCT spelling, ', ') AS "Other Regional Spelling" """
     if "Tree Type" in select_list:
-        statement += ",  GROUP_CONCAT(DISTINCT tree_type SEPARATOR ', ') AS `Tree Type`"
+        statement += """,  STRING_AGG(DISTINCT tree_type, ', ') AS "Tree Type" """
     if "Climatic Zone" in select_list:
-        statement += ", GROUP_CONCAT(DISTINCT climatic_zone SEPARATOR ', ') AS `Climatic Zone`"
+        statement += """, STRING_AGG(DISTINCT climatic_zone, ', ') AS "Climatic Zone" """
     if "Minimum Rainfall" in select_list:
-        statement += ", MIN(rainfall_min) AS `Minimum Rainfall`"
+        statement += ', MIN(rainfall_min) AS "Minimum Rainfall"'
     if "Maximum Rainfall" in select_list:
-        statement += ", Max(rainfall_max) AS `Maximum Rainfall`"
+        statement += ', Max(rainfall_max) AS "Maximum Rainfall"'
     if "Lowest Altitude" in select_list:
-        statement += ", MIN(altitude_min) AS `Lowest Altitude`"
+        statement += ', MIN(altitude_min) AS "Lowest Altitude"'
     if "Highest Altitude" in select_list:
-        statement += ", MAX(altitude_max) AS `Highest Altitude`"
+        statement += ', MAX(altitude_max) AS "Highest Altitude"'
     if "Utilities" in select_list or utility_list:
-        statement += """
-                        , GROUP_CONCAT(DISTINCT CONCAT(CASE 
+        statement += """, STRING_AGG(DISTINCT (CASE 
                             WHEN utility_usage = 1 THEN utility_name 
                             WHEN utility_usage = 2 THEN UPPER(utility_name)
-                            END) SEPARATOR ', ') as `Usage`
-                    """
+                            END), ', ') as "Usage" """
     return statement
 
 
 
-def produce_statement(english_list, somali_list, utility_list, select_list):
+def produce_statement(query_dict, select_list):
     statement = ""
 
-    statement += data_to_view(select_list, utility_list)
+    statement += data_to_view(select_list, query_dict["utilities"])
     statement += join_statement()
-
+    statement += where_statement(query_dict)
     statement += group_statement()
+
+    statement += order_statement()
+    print(statement)
     
 
     return statement
--- streamlit_dbms.py
+++ streamlit_dbms.py
@@ -1,9 +1,19 @@
 import streamlit as st
 import lists
 import produce_SQl_statement
+import psycopg2
 
 
-conn = st.experimental_connection("mysql", type="sql")
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+conn = init_connection()
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
 def main():
 
@@ -16,52 +26,64 @@ def main():
     if choice == "Home":
         st.subheader("Home")
     elif choice == "Search":
+
+        query_dict = {}
+
         st.subheader("Search")
         search_english = st.multiselect(
             'Select an English Name',
             lists.english_name_list
         )
+        query_dict["english"] = search_english
         
         search_somali = st.multiselect(
             'Select a Somali Name',
             lists.somali_name_list
         )
-        st.write('List:', search_somali)
+        query_dict["somali"] = search_somali
 
         search_arabic = st.multiselect(
             'Select an Arabic Name',
             lists.arabic_name_list
         )
+        query_dict["arabic"] = search_arabic
 
         search_tree_type = st.multiselect(
             'Select a Tree Type',
             lists.tree_type_list
         )
+        query_dict["type"] = search_tree_type
 
         search_climatic_zone = st.multiselect(
             'Select a Climatic Zone',
             lists.climatic_zone_list
         )
+        query_dict["climate"] = search_climatic_zone
 
         search_utilities = st.multiselect(
             'Select Utilities',
             lists.utilities_list
         )
+        query_dict["utilities"] = search_utilities
 
         
 
         search_rainfall = st.slider(
             'Select a range of values',
             0, 1500, (300, 1200))
-        st.write('Values:', search_rainfall)
+        st.write("If you don't want to limit the range, slide the points to each end")
+        query_dict["rainfall"] = search_rainfall 
+
         search_altitude = st.slider(
             'Select a range of values',
             0, 2500, (500, 2000))
-        st.write('Values:', search_altitude)
+        st.write("If you don't want to limit the range, slide the points to each end")
+        query_dict["altitude"] = search_altitude
         search_temperature = st.slider(
             'Select a range of values',
             15, 30, (20, 25))
-        st.write('Values:', search_temperature)
+        st.write("If you don't want to limit the range, slide the points to each end")
+        query_dict["temperature"] = search_temperature
 
         select_list = st.multiselect(
             'Select the Data You Want to See',
@@ -70,12 +92,11 @@ def main():
 
         if st.button('Search!'):
             
-            query_statement = produce_SQl_statement.produce_statement(search_english,
-                                                                      search_somali, 
-                                                                      search_utilities, 
+            query_statement = produce_SQl_statement.produce_statement(query_dict,
                                                                       select_list)
-            df = conn.query(query_statement, ttl=0)
-            st.table(df)
+            st.write(query_statement)
+            rows = run_query(query_statement)
+            st.table(rows)
             
 
     elif choice == "Add":
